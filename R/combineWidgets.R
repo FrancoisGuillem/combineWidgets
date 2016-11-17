6df86fce1eb3c7ab9e7baaa48a17bd99665d5c26
cuche27 <guillem_francois@yahoo.fr>
2016-11-17 22:20:20 +0100

    First commit

---
+++ R/combineWidgets.R
@@ -0,0 +1,60 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+combineWidgets <- function(..., width = NULL, height = NULL) {
+  widgets <- list(...)
+  data <- lapply(widgets, function(x) x$x)
+  widgetType <- sapply(widgets, function(x) class(x)[1])
+
+  x <- list(data = data, widgetType = widgetType);
+
+  # create widget
+  combinedWidget <- htmlwidgets::createWidget(
+    name = 'combineWidgets',
+    x,
+    width = width,
+    height = height,
+    package = 'combineWidgets'
+  )
+
+  deps <- lapply(widgets, function(x) {
+    append(getDependency(class(x)[1], attr(x, "package")), x$dependencies)
+  })
+  deps <- do.call(c, deps)
+
+  combinedWidget$dependencies <- deps
+
+  combinedWidget
+}
+
+#' Shiny bindings for combineWidgets
+#'
+#' Output and render functions for using combineWidgets within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a combineWidgets
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name combineWidgets-shiny
+#'
+#' @export
+combineWidgetsOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'combineWidgets', width, height, package = 'combineWidgets')
+}
+
+#' @rdname combineWidgets-shiny
+#' @export
+renderCombineWidgets <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, combineWidgetsOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/combineWidgets.js
@@ -0,0 +1,42 @@
+HTMLWidgets.widget({
+
+  name: 'combineWidgets',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    function getWidgetFactory(name) {
+      return HTMLWidgets.widgets.filter(function(x) {return x.name == name})[0];
+    }
+
+    return {
+
+      renderValue: function(x) {
+        var nWidgets = x.widgetType.length;
+        window.x = x;
+        // Initialize html
+        for (var i = 0; i < nWidgets; i++) {
+          el.innerHTML += '<div id="widget' + i + '" style="width:100%;height:' + (100 /nWidgets) + '%;float:left;"></div>';
+        }
+
+        for (i = 0; i < nWidgets; i++) {
+          var child = document.getElementById("widget" + i);
+          var widgetFactory = getWidgetFactory(x.widgetType[i]);
+          var w = widgetFactory.initialize(child, "100%", (100 /nWidgets) + "%");
+          widgetFactory.renderValue(child, x.data[i], w);
+        }
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
