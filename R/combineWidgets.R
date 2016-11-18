9a69c27b40f3d29e704c5c9467dbe444f15827f5
françois GUILLEM <francois.guillem@rte-france.com>
2016-11-18 11:51:27 +0100

    Add documentation

--- R/combineWidgets.R
+++ R/combineWidgets.R
@@ -1,12 +1,72 @@
-#' <Add Title>
+#' Combine several interactive plots
 #'
-#' <Add Description>
+#' This function combines different htmlwidgets in a unique view.
+#'
+#' @param ...
+#'   htmlwidgets to combine.
+#' @param nrow
+#'   Number of rows of the layout. If \code{NULL}, the function will automatically
+#'   take a value such that are at least as many cells in the layout as the number
+#'   of htmlwidgets.
+#' @param ncol
+#'   Number of columns of the layout.If \code{NULL}, the function will automatically
+#'   take a value such that are at least as many cells in the layout as the number
+#'   of htmlwidgets.
+#' @param title
+#'   Title of the view.
+#' @param rowsize
+#'   This argument controls the relative size of each row. For instance, if
+#'   the layout has two rows and \code{rowsize = c(2,1)}, then the width of the
+#'   first row will be twice the one of the second one. This argument is recycled
+#'   to fit the number of rows.
+#' @param colsize
+#'   Same as rowsize but for the height of the columns of the layout.
+#' @param byrow
+#'   If \code{TRUE}, then the layout is filled by row. Else it is filled by
+#'   column.
+#' @param titleCSS
+#'   A character containing css properties to modify the appearance of the title
+#'   of the view.
+#' @param width
+#'   Total width of the layout (optional, defaults to automatic sizing)
+#' @param height
+#'   Total height of the layout (optional, defaults to automatic sizing)
+#' @return
+#'   A HTML widget.
+#'
+#' @details
+#' The function only allows table like layout : each row has the same number of
+#' columns and reciprocally. But it is possible to create more complex layout
+#' by nesting combined htmlwidgets. (see examples)
+#'
+#' @examples
+#' if (require(plotly)) {
+#'   data(iris)
+#'
+#'   combineWidgets(title = "The Iris dataset",
+#'     plot_ly(iris, x = ~Sepal.Length, type = "histogram", nbinsx = 20),
+#'     plot_ly(iris, x = ~Sepal.Width, type = "histogram", nbinsx = 20),
+#'     plot_ly(iris, x = ~Petal.Length, type = "histogram", nbinsx = 20),
+#'     plot_ly(iris, x = ~Petal.Width, type = "histogram", nbinsx = 20)
+#'   )
+#'
+#'   # Create a more complex layout by nesting combinedWidgets
+#'   combineWidgets(title = "The iris data set: sepals", ncol = 2, colsize = c(2,1),
+#'     plot_ly(iris, x = ~Sepal.Length, y = ~Sepal.Width, type = "scatter",
+#'             mode = "markers", color = ~Species),
+#'     combineWidgets(
+#'       plot_ly(iris, x = ~Sepal.Length, type = "histogram", nbinsx = 20),
+#'       plot_ly(iris, x = ~Sepal.Width, type = "histogram", nbinsx = 20)
+#'     )
+#'   )
+#' }
 #'
 #' @import htmlwidgets
 #'
 #' @export
 combineWidgets <- function(..., nrow = NULL, ncol = NULL, title = NULL,
                            rowsize = 1, colsize = 1, byrow = TRUE,
+                           titleCSS = "",
                            width = NULL, height = NULL) {
   widgets <- lapply(list(...), function(x) {
     if (is.null(x$preRenderHook)) return(x)
@@ -66,7 +126,8 @@ combineWidgets <- function(..., nrow = NULL, ncol = NULL, title = NULL,
                      dirClass, paste(rowsEl, collapse = ""))
 
   if(!is.null(title) && !title == "") {
-    titleEl <- sprintf('<div><h2 class="cw-title">%s</h2></div>', title)
+    titleEl <- sprintf('<div><h2 class="cw-title" style="%s">%s</h2></div>',
+                       titleCSS, title)
   } else {
     titleEl <- ""
   }
